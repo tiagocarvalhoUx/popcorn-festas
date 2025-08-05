691a0022920f59b03f50c9159ee52eb0d4e16d51
nome <email>
2025-08-05 17:08:25 -0300

    file-tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { StrictMode } from 'react'
 import "./index.css";
-import App from "./App.tsx";
+import App from "./App";
 import { createRoot } from 'react-dom/client'
 
 createRoot(document.getElementById('root')).render(
